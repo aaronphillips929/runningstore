562df5abf3633c4c9032ddc4c25f0f85b7ae9029
Aaron Phillips <aaron.phillips929@gmail.com>
2019-10-10 20:41:32 -0500

    Update bamazonSchema.sql

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
 CREATE TABLE products (
     item_id INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
     product_name VARCHAR (300) NOT NULL,
@@ -5,25 +9,18 @@ CREATE TABLE products (
     price FLOAT(10, 2) NOT NULL, 
     stock_quantity INTEGER (75) NOT NULL,
     PRIMARY KEY (item_id)
-)
+);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("GU", "Nutrition", "5", "100")
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Nuun Tablets", "Nutrition", "0.50", "300")
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("On Running Cloudstratus", "Shoes", "189", "10")
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Hoka Carbon X", "Shoes", "190", "5")
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Lululemon Metalvent Tech", "Shirts", "68", "10")
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Tracksmith 5 Inch Shorts", "Shorts", "80", "10")
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Ciele Hat", "Hats", "40", "5")
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("On Running Tall Socks", "Socks", "40", "4")
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Chicago Marathon Entry", "Misc", "500", "1")
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Sub 2 Hour Marathon", "Not Possible", "0", "2")
+VALUES 
+("GU", "Nutrition", "5", "100"),
+("Nuun Tablets", "Nutrition", "0.50", "300"),
+("On Running Cloudstratus", "Shoes", "189", "10"),
+("Hoka Carbon X", "Shoes", "190", "5"),
+("Lululemon Metalvent Tech", "Shirts", "68", "10"),
+("Tracksmith 5 Inch Shorts", "Shorts", "80", "10"),
+("Ciele Hat", "Hats", "40", "5"),
+("On Running Tall Socks", "Socks", "40", "4"),
+("Chicago Marathon Entry", "Misc", "500", "1"),
+("Sub 2 Hour Marathon", "Not Possible", "0", "2");
+SELECT * from products;
